bbd89f293c0e58ae9471733e8ea91a8aa6fb28f8
Kruti Shah <krutishah@MacBook-Pro.fios-router.home>
2020-07-09 00:40:01 -0400

    Added billing and client side app

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
+const bodyParser = require('body-parser');
 const keys = require('./config/keys')
 require('./models/user');
 require('./services/passport');
@@ -10,6 +11,7 @@ mongoose.connect(keys.mongoURI);
 
 const app = express();
 
+app.use(bodyParser.json());
 //use of cookieSession
 app.use(
   cookieSession({
@@ -21,6 +23,18 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app);
+
+if(process.env.NODE_ENV === 'production'){
+  //Express will serve up production asssets like main.js or mains.class
+  app.use(espress.static('client/build'));
+
+  //Express will serve up index.html if it doesn't recognize the route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT||5000;
 app.listen(PORT);
--- services/passport.js
+++ services/passport.js
@@ -22,18 +22,16 @@ passport.use(
     callbackURL : '/auth/google/callback',
     proxy : true
   },
-  (accessToken, refreshToken, profile, done) => {
-    User.findOne({ googleId: profile.id}).then((existingUser) => {
-        if(existingUser){
-          //we already have a record with the given id
-          done(null, existingUser);
-        } else{
-          //we dont have a user record with this given id
-          new User({ googleId: profile.id })
-            .save() //new instance of a user
-            .then(user => done(null, user));
-        }
-      });
+  async (accessToken, refreshToken, profile, done) => {
+    const existingUser = await User.findOne({ googleId: profile.id});
+      if(existingUser){
+        //we already have a record with the given id
+        return done(null, existingUser);
+      }
+      
+      //we dont have a user record with this given id
+      const user = await new User({ googleId: profile.id }).save(); //new instance of a user
+      done(null, user);
     }
   )
 );
